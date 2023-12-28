1fd61d18b28e522a4347ad7e8dcc249b9f74b7a1
RomanMatkovskyi <roman.matkovskyii@gmail.com>
2023-12-28 11:57:11 +0100

    Initial commit

---
+++ nodejs_hw01/db/contacts.js
@@ -0,0 +1,60 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
+const crypto = require('node:crypto');
+
+const contactsPath = path.join(__dirname, 'contacts.json');
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, { encoding: 'utf-8' });
+
+  return JSON.parse(data);
+}
+
+async function writeContacts(contact) {
+  return fs.writeFile(contactsPath, JSON.stringify(contact, undefined, 2));
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find(contact => contact.id === contactId);
+
+  if (contact === undefined) {
+    return null;
+  }
+
+  return contact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(contact => contact.id === contactId);
+
+  if (index === -1) {
+    return null;
+  }
+
+  const newContacts = [
+    ...contacts.slice(0, index),
+    ...contacts.slice(index + 1),
+  ];
+  await writeContacts(newContacts);
+
+  return contacts[index];
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { name, email, phone, id: crypto.randomUUID() };
+
+  contacts.push(newContact);
+  await writeContacts(contacts);
+
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
